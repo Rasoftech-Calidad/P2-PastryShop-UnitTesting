bfa3075bc9b1f0a4e505540ae65555c4dbf29b3a
ASDIO1 <diego.an.sanchez.a@gmail.com>
2023-04-09 17:41:52 -0400

    Added test that ensures Register model creation
    - This model class now has full coverage

---
+++ Backend/PastryShopAPI/UnitTesting/ModelsTest/RegisterViewModelShould.cs
@@ -0,0 +1,25 @@
+﻿using PastryShopAPI.Models.Security;
+
+namespace UnitTesting.ModelsTest
+{
+    public class RegisterViewModelShould
+    {
+        [Theory]
+        [InlineData("valid@valid.com", "validpassword", "validpassword")]
+        public void CreateRegisterModel_WithValidData_ReturnsValidObject(string email, string password, string confirmpassword)
+        {
+            // Arrange
+            // Act
+            var registerUser = new RegisterViewModel()
+            {
+                Email = email,
+                Password = password,
+                ConfirmPassword = confirmpassword,
+            };
+            // Assert
+            Assert.Equal(registerUser.Email, email);
+            Assert.Equal(registerUser.Password, password);
+            Assert.Equal(registerUser.ConfirmPassword, confirmpassword);
+        }
+    }
+}
