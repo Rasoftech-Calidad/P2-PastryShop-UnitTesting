9cd7b1af2a0e43e0e8308ef70db35c4bb53fcf33
Alfred <alfred.garcia@ucb.edu.bo>
2023-04-09 00:44:59 -0400

    ValidateCategorySetId AND ValidateCategorySetName

---
+++ Backend/PastryShopAPI/UnitTesting/DataTest/EntitiesTest/CategoryEntityShould.cs
@@ -0,0 +1,45 @@
+﻿using PastryShopAPI;
+using PastryShopAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.DataTest.EntitiesTest
+{
+    public class CategoryEntityShould
+    {
+        [Fact]
+        public void ValidateCategorySetId()
+        {
+            //Arrange
+            var category = new CategoryEntity
+            {
+                Id = 1,
+            };
+
+            //Act
+            var getValue = category.Id;
+
+            //Assert
+            Assert.Equal(1, getValue);
+        }
+
+        [Fact]
+        public void ValidateCategorySetName()
+        {
+            //Arrange
+            CategoryEntity category = new CategoryEntity();
+            string name = "Pastel";
+            category.Name = name;
+
+            //Act
+            var getValue = category.Name;
+
+            //Assert
+            //Assert.Equal(product.Name, name);
+            Assert.Contains(getValue, category.Name);
+        }
+    }
+}
