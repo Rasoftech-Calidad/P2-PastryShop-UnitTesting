40511cf2c2e10582cf27e2ecd708f1f01f6bc879
Alfred <alfred.garcia@ucb.edu.bo>
2023-04-09 21:47:36 -0400

    ValidateDescription AND ValidateImageUrl

--- Backend/PastryShopAPI/UnitTesting/ModelsTest/CategoryModelShould.cs
+++ Backend/PastryShopAPI/UnitTesting/ModelsTest/CategoryModelShould.cs
@@ -41,5 +41,39 @@ namespace UnitTesting.ModelsTest
             Assert.Equal(category.Name, name);
             //Assert.Contains(getValue, category.Name);
         }
+
+        [Fact]
+        public void ValidateDescription()
+        {
+            //Arrange
+            CategoryModel category = new CategoryModel();
+            category.Description = "Pasteles finos en corteza doble";
+
+            //Act
+            var getValue = category.Description;
+
+            //Assert
+            //Assert.IsType<string>(getValue);
+            Assert.Contains("Pasteles finos en corteza doble", getValue);
+        }
+
+        [Fact]
+        public void ValidateImageUrl()
+        {
+            //Arrange
+            CategoryModel category = new CategoryModel();
+            category.ImageUrl = "https://st4.depositphotos.com/21419180/23161/i/600/depositphotos_231611588-stock-photo-apple-pie-double-crust-apple.jpg";
+
+            //Act
+            var listExtencionImage = new List<string>() { ".jpg" };
+
+            //Assert
+            Assert.Contains(".jpg", category.ImageUrl);
+            /*foreach (string extencion in listExtencionImage)
+            {
+                Assert.Contains(extencion, category.ImageUrl);
+            }*/
+            //Assert.IsType<string>(category.ImageUrl);
+        }
     }
 }
