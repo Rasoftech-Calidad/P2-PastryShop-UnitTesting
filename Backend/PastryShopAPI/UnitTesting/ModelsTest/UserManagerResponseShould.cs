ce48c5eb8d9b3caf2e2e10afd017ee49ea3161d7
ASDIO1 <diego.an.sanchez.a@gmail.com>
2023-04-13 16:29:28 -0400

    Test setting true and false to isSuccess - passed

--- Backend/PastryShopAPI/UnitTesting/ModelsTest/UserManagerResponseShould.cs
+++ Backend/PastryShopAPI/UnitTesting/ModelsTest/UserManagerResponseShould.cs
@@ -23,5 +23,29 @@ namespace UnitTesting.ModelsTest
             // Assert
             Assert.Equal(message, _userManagerResponse.Message);
         }
+
+        [Fact]
+        public void SetSuccessTrue_WithValidData_ReturnsSameTrue()
+        {
+            // Arrange
+            var isSuccess = true;
+            // Act
+            _userManagerResponse.IsSuccess = isSuccess;
+
+            // Assert
+            Assert.True(isSuccess);
+        }
+
+        [Fact]
+        public void SetSuccessFalse_WithValidData_ReturnsSameFalse()
+        {
+            // Arrange
+            var isSuccess = false;
+            // Act
+            _userManagerResponse.IsSuccess = isSuccess;
+
+            // Assert
+            Assert.False(isSuccess);
+        }
     }
 }
