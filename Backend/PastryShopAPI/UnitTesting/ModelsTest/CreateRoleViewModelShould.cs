f9134aef51c13937a3784f653090a0f645d07f84
ASDIO1 <diego.an.sanchez.a@gmail.com>
2023-04-13 13:32:17 -0400

    Test setting Name

---
+++ Backend/PastryShopAPI/UnitTesting/ModelsTest/CreateRoleViewModelShould.cs
@@ -0,0 +1,32 @@
+﻿using PastryShopAPI.Models.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.ModelsTest
+{
+    public class CreateRoleViewModelShould
+    {
+        CreateRoleViewModel _createRoleModel;
+        public CreateRoleViewModelShould()
+        {
+            _createRoleModel = new CreateRoleViewModel();
+        }
+
+        [Fact]
+        public void SetName_WithValidData_ReturnsValidName()
+        {
+            // Arrange
+            string name = "Name";
+
+            // Act
+            _createRoleModel.Name = name;
+
+            // Assert
+            Assert.Equal(name, _createRoleModel.Name);
+        }
+        
+    }
+}
