0359849fa597d68eefddee2e5fc70be7aede86d7
ASDIO1 <diego.an.sanchez.a@gmail.com>
2023-04-13 16:29:07 -0400

    Test setting message passed

---
+++ Backend/PastryShopAPI/UnitTesting/ModelsTest/UserManagerResponseShould.cs
@@ -0,0 +1,27 @@
+﻿using PastryShopAPI.Models.Security;
+
+
+namespace UnitTesting.ModelsTest
+{
+    public class UserManagerResponseShould
+    {
+        UserManagerResponse _userManagerResponse;
+        public UserManagerResponseShould()
+        {
+            _userManagerResponse = new UserManagerResponse();
+        }
+
+        [Fact]
+        public void SetMessage_WithValidData_ReturnsValidMessage()
+        {
+            // Arrange
+            string message = "Some model does not exist";
+
+            // Act
+            _userManagerResponse.Message = message;
+
+            // Assert
+            Assert.Equal(message, _userManagerResponse.Message);
+        }
+    }
+}
