bd9622aea9ee1cb7dccdec6604b092a023e4de50
melanieSasari <melsasarialborta@gmail.com>
2023-04-13 23:45:58 -0400

    Implementation of unit tests for var id

---
+++ Backend/PastryShopAPI/UnitTesting/DataTest/EntitiesTest/ProductComboEntityShould.cs
@@ -0,0 +1,29 @@
+﻿using PastryShopAPI.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.DataTest.EntitiesTest
+{
+    public class ProductComboEntityShould
+    {
+
+        [Fact]
+        public void ValidateComboEntity_SetId()
+        {
+            //Arrange
+            var product = new Product_ComboEntity
+            {
+                Id = 1,
+            };
+
+            //Act
+            var getValue = product.Id;
+
+            //Assert
+            Assert.Equal(1, getValue);
+        }
+    }
+}
